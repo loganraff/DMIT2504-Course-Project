f9cd1870af3a7879ad7e16e1b18a4aa6c134fb2d
loganraff <122321572+loganraff@users.noreply.github.com>
2024-04-23 14:27:53 -0600

    Created home page for app

---
+++ bball_stats_application/lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../services/database_service.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final DatabaseService _databaseService = DatabaseService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
