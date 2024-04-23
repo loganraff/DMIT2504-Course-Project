35d2c02a88581096c02d064d74553c9d8eae9710
loganraff <122321572+loganraff@users.noreply.github.com>
2024-04-23 14:24:08 -0600

    Created Database service for games

---
+++ bball_stats_application/lib/services/database_service.dart
@@ -0,0 +1,26 @@
+import 'package:bball_stats_application/models/game.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+const String GAME_COLLECTION = 'games';
+
+class DatabaseService {
+  final _firestore = FirebaseFirestore.instance;
+
+  late final CollectionReference _gamesRef;
+
+  DatabaseService() {
+    _gamesRef = _firestore.collection(GAME_COLLECTION).withConverter<Game>(
+        fromFirestore: (snapshots, _) => Game.fromJson(
+              snapshots.data()!,
+            ),
+        toFirestore: (game, _) => game.toJson());
+  }
+
+  Stream<QuerySnapshot> getGames() {
+    return _gamesRef.snapshots();
+  }
+
+  void addGame(Game game) async {
+    _gamesRef.add(game);
+  }
+}
