5435782a6adecab5f6e418e86bf63374dedcc888
loganraff <122321572+loganraff@users.noreply.github.com>
2024-04-23 18:14:15 -0600

    Added animated icon on the landing page

--- bball_stats_application/lib/pages/landing_page.dart
+++ bball_stats_application/lib/pages/landing_page.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:bball_stats_application/pages/home_page.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 10),
+    );
+    _animation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(_controller);
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,17 +41,24 @@ class LandingPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Welcome to BBall Stats!',
+              'Welcome to BBall Stats!', // Add a title welcoming the user
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20),
-            Icon(
-              Icons.sports_basketball,
-              size: 50.0,
-              color: Colors.orange,
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2 * 3.14, // Rotate 360 degrees
+                  child: Icon(
+                    Icons.sports_basketball,
+                    size: 50.0,
+                    color: Colors.orange,
+                  ),
+                );
+              },
             ),
             SizedBox(height: 20),
             ElevatedButton(
