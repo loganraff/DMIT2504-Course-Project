5d28c04c2db8b2b2f6504d6610dbd021062bc900
loganraff <122321572+loganraff@users.noreply.github.com>
2024-04-23 18:06:22 -0600

    changes to landing page

--- bball_stats_application/lib/pages/landing_page.dart
+++ bball_stats_application/lib/pages/landing_page.dart
@@ -20,21 +20,27 @@ class LandingPage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
+            Icon(
+              Icons.sports_basketball,
+              size: 50.0,
+              color: Colors.orange,
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-              Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-              );
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
               },
               style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.orange, // Set the background color to orange
+                backgroundColor: Colors.orange, // Set the background color to orange
               ),
               child: Text(
-              'View your Stats',
-              style: TextStyle(
-              color: Colors.black,
-              ),
+                'View your Stats',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
               ),
             ),
           ],
